4e1d0a1552ed432c31c29b9fb6c96f335bdb5c55
nehamanghnani01 <nehajm@tamu.edu>
2025-04-02 01:34:59 -0500

    endpoints added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js";
+import Product from './models/product.model.js';
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -10,7 +12,20 @@ app.get("/", (req,res) => {
     res.send("server is ready!")
 })
 
-app.post("/products", async(req, res) => {
+app.use(express.json()) // allows to accept JSON data in the req body
+
+//show all the products till now
+app.get("/api/products", async(req,res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({success: true, message: products})
+    } catch(error) {
+        console.log("Error = ",error)
+        res.status(500).json({success: false, message: "Server error"})
+    }
+})
+
+app.post("/api/products", async(req, res) => {
     const product = req.body //user input 
     if(!product.name || !product.price || !product.image) {
         return res.status(400).json({success: false, message: "Please send all the fields"});
@@ -27,7 +42,38 @@ app.post("/products", async(req, res) => {
     }
 })
 
+//update a product 
+app.put("/api/products/:id", async(req,res)=> {
+    const {id} = req.params
+    console.log("id = ",id)
+    const product = req.body
 
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({success: false, message: "Invalid Product ID"})
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true});
+        res.status(200).json({success: true, message: updatedProduct})
+    } catch(error) {
+        console.log("Error =", error)
+        res.status(500).json({success: false, message: "Server Error"})
+    }
+})
+
+//delete the product - to delete we have to pass the 'id' of the product 
+app.delete("/api/products/:id", async(req,res)=> {
+    const {id} = req.params
+    console.log("id is",id)
+
+    try {
+        await Product.findByIdAndDelete(id)
+        res.status(200).json({success: true, message:"Product deleted"})
+    } catch(error){
+        console.log("error - ", error);
+        res.status(400).json({success:false, message: "Product not found!"})
+    }
+})
 
 console.log(process.env.MONGO_URI)  //the variables in .env can be accessed only when the dotenc module is imported
 
